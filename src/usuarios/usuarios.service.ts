7618a713e17d54a5ea5ae19f0bac06baf54ed30b
palma26 <kevinpalma2627@outlook.com>
2024-10-15 15:08:29 -0600

    cambios

--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -120,18 +120,20 @@ export class UsuariosService {
 
     try {
 
-      let usuario: Usuarios;
+      // let usuario: Usuarios;
 
-      if (this.isEmail(user)) {
-        usuario = await this.usuariosRepository.findOne({
-          where: { email: user },
-        });
-      } else if (this.isPhoneNumber(user)) {
-        usuario = await this.usuariosRepository.findOne({
-          where: { telefono: user },
-        });
-      }
+      const usuario = await this.usuariosRepository.findOneBy({telefono: user});
 
+      // if (this.isEmail(user)) {
+      //   usuario = await this.usuariosRepository.findOne({
+      //     where: { email: user },
+      //   });
+      // } else if (this.isPhoneNumber(user)) {
+      //   usuario = await this.usuariosRepository.findOne({
+      //     where: { telefono: user },
+      //   });
+      // }
+ 
       if (!usuario) {
         throw new NotFoundException(`Usuario con identificador ${user} no encontrado`);
       }
