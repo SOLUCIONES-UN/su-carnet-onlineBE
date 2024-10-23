7bf5f33ef69686ef19243d199e70c1621e2a1820
JEstivenA <estiven6647@gmail.com>
2024-10-23 10:05:36 -0600

    change response of method delete group

--- src/sucursales_areas_grupos_informacion/sucursales_areas_grupos_informacion.service.ts
+++ src/sucursales_areas_grupos_informacion/sucursales_areas_grupos_informacion.service.ts
@@ -1,4 +1,10 @@
-import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateSucursalesAreasGruposInformacionDto } from './dto/create-sucursales_areas_grupos_informacion.dto';
 import { UpdateSucursalesAreasGruposInformacionDto } from './dto/update-sucursales_areas_grupos_informacion.dto';
 import { SucursalesAreasInformacion } from '../entities/SucursalesAreasInformacion';
@@ -10,8 +16,9 @@ import { GenericResponse } from '../common/dtos/genericResponse.dto';
 
 @Injectable()
 export class SucursalesAreasGruposInformacionService {
-
-  private readonly logger = new Logger("SucursalesAreasGruposInformacionService");
+  private readonly logger = new Logger(
+    'SucursalesAreasGruposInformacionService',
+  );
 
   constructor(
     @InjectRepository(SucursalesAreasInformacion)
@@ -19,18 +26,25 @@ export class SucursalesAreasGruposInformacionService {
 
     @InjectRepository(SucursalesAreasGruposInformacion)
     private SucursalesAreasGruposRepository: Repository<SucursalesAreasGruposInformacion>,
+  ) {}
 
-  ) { }
-
-  async create(createSucursalesAreasGruposInformacionDto: CreateSucursalesAreasGruposInformacionDto) {
-
+  async create(
+    createSucursalesAreasGruposInformacionDto: CreateSucursalesAreasGruposInformacionDto,
+  ) {
     try {
+      const { idSucursalArea, ...infoData } =
+        createSucursalesAreasGruposInformacionDto;
 
-      const { idSucursalArea, ...infoData } = createSucursalesAreasGruposInformacionDto;
+      const area_sucursal = await this.sucursalesAreasRepository.findOneBy({
+        id: idSucursalArea,
+      });
 
-      const area_sucursal = await this.sucursalesAreasRepository.findOneBy({ id: idSucursalArea });
-
-      if (!area_sucursal)  return new GenericResponse('400', `area_sucursal con ID ${idSucursalArea} no encontrada`, []); 
+      if (!area_sucursal)
+        return new GenericResponse(
+          '400',
+          `area_sucursal con ID ${idSucursalArea} no encontrada`,
+          [],
+        );
 
       const areaGrupoSucursal = this.SucursalesAreasGruposRepository.create({
         ...infoData,
@@ -40,56 +54,55 @@ export class SucursalesAreasGruposInformacionService {
       await this.SucursalesAreasGruposRepository.save(areaGrupoSucursal);
 
       return new GenericResponse('200', `EXITO`, areaGrupoSucursal);
-
     } catch (error) {
       return new GenericResponse('500', `Error`, error);
     }
-
   }
 
   async findAll() {
-
     try {
-      
-      const areaaGruposSucursal = await this.SucursalesAreasGruposRepository.find({
-        where: { estado: 1 },
-        relations: ['idSucursalArea'],
-      });
-  
-      return new GenericResponse('200', `EXITO`, areaaGruposSucursal);
+      const areaaGruposSucursal =
+        await this.SucursalesAreasGruposRepository.find({
+          where: { estado: 1 },
+          relations: ['idSucursalArea'],
+        });
 
+      return new GenericResponse('200', `EXITO`, areaaGruposSucursal);
     } catch (error) {
       return new GenericResponse('500', `Error`, error);
     }
   }
 
-  async findAllByAreaInformacionId(idSucursalArea:number) {
-
+  async findAllByAreaInformacionId(idSucursalArea: number) {
     try {
-      
-      const sucursalAreaInformacion = await this.sucursalesAreasRepository.findOne({
-        where: {estado:1, id: idSucursalArea}
-      });
-  
-      if(!sucursalAreaInformacion) return new GenericResponse('400', `sucursalAreaInformacion con ID ${idSucursalArea} no encontrada puede estar inactivo`, []); 
-  
-      const SucursalesAreasGrupos = await this.SucursalesAreasGruposRepository.find({
-        where: { idSucursalArea: sucursalAreaInformacion, estado: 1 },
-        relations: ['idSucursalArea'],
-      });
-  
-      return new GenericResponse('200', `EXITO`, SucursalesAreasGrupos);
+      const sucursalAreaInformacion =
+        await this.sucursalesAreasRepository.findOne({
+          where: { estado: 1, id: idSucursalArea },
+        });
 
+      if (!sucursalAreaInformacion)
+        return new GenericResponse(
+          '400',
+          `sucursalAreaInformacion con ID ${idSucursalArea} no encontrada puede estar inactivo`,
+          [],
+        );
+
+      const SucursalesAreasGrupos =
+        await this.SucursalesAreasGruposRepository.find({
+          where: { idSucursalArea: sucursalAreaInformacion, estado: 1 },
+          relations: ['idSucursalArea'],
+        });
+
+      return new GenericResponse('200', `EXITO`, SucursalesAreasGrupos);
     } catch (error) {
       return new GenericResponse('500', `Error`, error);
     }
   }
 
   async findOne(id: number) {
-    
     try {
-      
-      const SucursalesAreasGrupos = this.SucursalesAreasGruposRepository.findOneBy({ id });
+      const SucursalesAreasGrupos =
+        this.SucursalesAreasGruposRepository.findOneBy({ id });
 
       return new GenericResponse('200', `EXITO`, SucursalesAreasGrupos);
     } catch (error) {
@@ -97,51 +110,70 @@ export class SucursalesAreasGruposInformacionService {
     }
   }
 
-  async update(id: number, updateSucursalesAreasGruposInformacionDto: UpdateSucursalesAreasGruposInformacionDto) {
-
+  async update(
+    id: number,
+    updateSucursalesAreasGruposInformacionDto: UpdateSucursalesAreasGruposInformacionDto,
+  ) {
     try {
-      const { idSucursalArea, ...infoData } = updateSucursalesAreasGruposInformacionDto;
+      const { idSucursalArea, ...infoData } =
+        updateSucursalesAreasGruposInformacionDto;
 
-      const areaaGruposSucursal = await this.SucursalesAreasGruposRepository.findOneBy({ id });
+      const areaaGruposSucursal =
+        await this.SucursalesAreasGruposRepository.findOneBy({ id });
 
-      if (!areaaGruposSucursal) return new GenericResponse('400', `areaaGruposSucursal con ID ${id} no encontrada`, []); 
+      if (!areaaGruposSucursal)
+        return new GenericResponse(
+          '400',
+          `areaaGruposSucursal con ID ${id} no encontrada`,
+          [],
+        );
 
-      const areasucursal = await this.sucursalesAreasRepository.findOneBy({ id: idSucursalArea });
-
-      if (!areasucursal) return new GenericResponse('400', `areasucursal con ID ${idSucursalArea} no encontrada`, []); 
-
-      const updateAreaGrupoSucursal = this.SucursalesAreasGruposRepository.merge(areaaGruposSucursal, {
-        ...infoData,
-        idSucursalArea: areasucursal
+      const areasucursal = await this.sucursalesAreasRepository.findOneBy({
+        id: idSucursalArea,
       });
+
+      if (!areasucursal)
+        return new GenericResponse(
+          '400',
+          `areasucursal con ID ${idSucursalArea} no encontrada`,
+          [],
+        );
+
+      const updateAreaGrupoSucursal =
+        this.SucursalesAreasGruposRepository.merge(areaaGruposSucursal, {
+          ...infoData,
+          idSucursalArea: areasucursal,
+        });
 
       // Guardar los cambios en la base de datos
       await this.SucursalesAreasGruposRepository.save(updateAreaGrupoSucursal);
 
       return new GenericResponse('200', `EXITO`, updateAreaGrupoSucursal);
-
     } catch (error) {
       return new GenericResponse('500', `Error`, error);
     }
-
   }
 
   async remove(id: number) {
-
     try {
-
-      const areaGruposSucursal = await this.SucursalesAreasGruposRepository.findOneBy({ id });
+      const areaGruposSucursal =
+        await this.SucursalesAreasGruposRepository.findOneBy({ id });
 
       if (!areaGruposSucursal) {
-        throw new NotFoundException(`areaGruposSucursal con ID ${id} no encontrada`);
+        throw new NotFoundException(
+          `areaGruposSucursal con ID ${id} no encontrada`,
+        );
       }
 
       areaGruposSucursal.estado = 0;
-      return await this.SucursalesAreasGruposRepository.save(areaGruposSucursal);
 
+      const groupDeleted = await this.SucursalesAreasGruposRepository.save(
+        areaGruposSucursal,
+      );
+
+      return new GenericResponse('200', `EXITO`, groupDeleted);
     } catch (error) {
       return new GenericResponse('500', `Error`, error);
     }
   }
-
 }
